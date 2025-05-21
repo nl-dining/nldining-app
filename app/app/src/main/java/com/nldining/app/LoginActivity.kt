b561f6031bdb7bdea8aac7ae1f88d98b84f57bc6
babacool126 <75642109+babacool126@users.noreply.github.com>
2025-05-21 23:17:37 +0200

    login_test_structure - added kotlin file in java

---
+++ app/app/src/main/java/com/nldining/app/LoginActivity.kt
@@ -0,0 +1,12 @@
+// app/src/main/java/com/nldining/app/LoginActivity.kt
+package com.nldining.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
