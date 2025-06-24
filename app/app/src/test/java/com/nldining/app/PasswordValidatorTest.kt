60158fd97ebf4da74c73472a1656f095f9d01fae
gjbakker <g.j.bakker@st.hanze.nl>
2025-06-24 11:05:39 +0200

    changed the password term and removed and label from the AndroidManifest.xml

--- app/app/src/test/java/com/nldining/app/PasswordValidatorTest.kt
+++ app/app/src/test/java/com/nldining/app/PasswordValidatorTest.kt
@@ -7,37 +7,37 @@ class PasswordValidatorTest {
 
     @Test
     fun validPassword_returnsTrue() {
-        val password = "Strong1!"
-        assertTrue(PasswordValidator.isValid(password))
+        val testPswd = "Strong1!"
+        assertTrue(PasswordValidator.isValid(testPswd))
     }
 
     @Test
     fun tooShortPassword_returnsFalse() {
-        val password = "S1!"
-        assertFalse(PasswordValidator.isValid(password))
+        val testPswd = "S1!"
+        assertFalse(PasswordValidator.isValid(testPswd))
     }
 
     @Test
     fun missingUppercase_returnsFalse() {
-        val password = "strong1!"
-        assertFalse(PasswordValidator.isValid(password))
+        val testPswd = "strong1!"
+        assertFalse(PasswordValidator.isValid(testPswd))
     }
 
     @Test
     fun missingLowercase_returnsFalse() {
-        val password = "STRONG1!"
-        assertFalse(PasswordValidator.isValid(password))
+        val testPswd = "STRONG1!"
+        assertFalse(PasswordValidator.isValid(testPswd))
     }
 
     @Test
     fun missingDigit_returnsFalse() {
-        val password = "Strong!"
-        assertFalse(PasswordValidator.isValid(password))
+        val testPswd = "Strong!"
+        assertFalse(PasswordValidator.isValid(testPswd))
     }
 
     @Test
     fun missingSpecialChar_returnsFalse() {
-        val password = "Strong1A"
-        assertFalse(PasswordValidator.isValid(password))
+        val testPswd = "Strong1A"
+        assertFalse(PasswordValidator.isValid(testPswd))
     }
 }
